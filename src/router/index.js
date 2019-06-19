7b32d8f5b254561b06d7c9c8024798fa0d5abf1b
BaiWeiJieKu <2534964002@qq.com>
2019-06-19 16:25:34 +0800

    异步

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,5 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from '@/pages/home/Home'
-import City from '@/pages/city/City'
-import Detail from '@/pages/detail/Detail'
 
 Vue.use(Router)
 
@@ -11,18 +8,19 @@ export default new Router({
     {
       path: '/',
       name: 'Home',
-      component: Home
+      //异步加载，进入页面再加载页面数据
+      component: () => import('@/pages/home/Home')
     },
     //添加一个城市路由
     {
     	path: '/city',
       name: 'City',
-      component: City
+      component: () => import('@/pages/city/City')
     },
     {
     	path: '/detail/:id',
       name: 'Detail',
-      component: Detail
+      component: () => import('@/pages/detail/Detail')
     }
   ],
   scrollBehavior (to, from, savedPosition) {
